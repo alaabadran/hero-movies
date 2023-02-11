bacdfdb86777e1289c69252875eab97dd8fa2806
Alaa Badran <me@alaabadran.com>
2023-02-11 16:31:43 +0300

    Define types and add super heros json file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+// import type { GetStaticProps, NextPage } from 'next';
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const apikey = "977fd4d4";
 
 export default function Home() {
   return (
---
+++ types/movie.ts
@@ -0,0 +1,49 @@
+export type APIKEY = string;
+
+interface RatingsType {
+  source: string;
+  value: string;
+}
+
+/*[
+  {
+    Source: "Internet Movie Database";
+    Value: "5.6/10";
+  },
+  {
+    Source: "Rotten Tomatoes";
+    Value: "62%";
+  },
+  {
+    Source: "Metacritic";
+    Value: "54/100";
+  }
+];*/
+
+export interface Movie {
+  title: string;
+  year: number;
+  rated: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  plot: string;
+  language: string;
+  country: string;
+  awards: string;
+  poster: string;
+  ratings: Array<RatingsType>;
+  metascore: number;
+  imdbRating: number; // 5.6
+  imdbVotes: string | number; //"270,620"
+  imdbID: string;
+  type: string;
+  DVD: string;
+  BoxOffice: string;
+  production: string;
+  website: string;
+  response: boolean;
+}
