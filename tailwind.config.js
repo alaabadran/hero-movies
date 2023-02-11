382cfdff54b8f37974e661cf12fcccacc0e68d7f
Alaa Badran <me@alaabadran.com>
2023-02-11 23:42:50 +0300

    Init tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
+    screens: {
+      "2xs": "375px",
+      xs: "428px",
+      sm: "744px",
+      md: "834px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1920px",
+    },
+    container: {
+      center: true,
+      screens: { "2xl": "1440px" },
+    },
   },
   plugins: [],
-}
+};
