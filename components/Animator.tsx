dd8b2bf5ed873a510823750e9005eee493eda68a
Alaa Badran <me@alaabadran.com>
2023-02-12 22:36:10 +0300

    Add children type

--- components/Animator.tsx
+++ components/Animator.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-const Animator = ({ children }) => (
+const Animator: React.FC<{ children: any }> = ({ children }) => (
   <motion.div
     initial={{ x: 300, opacity: 0 }}
     animate={{ x: 0, opacity: 1 }}
